8bdfed4395cf708979bbd3179509316eb1959b5b
MarcusMBP <marcusmbp@MarcusMBPde-MacBook-Pro.local>
2020-11-05 18:36:16 +0100

    update

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-const reference = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+const reference;
 
 const rgbToHex = function (input) {
 	let firstValue;
@@ -34,5 +34,4 @@ const rgbHexConverter = function (input) {
 	}
 }
 
-rgbHexConverter('rgb(255,255,255');
 
